6f549c064dcef74b5ac1401ffec05201bc717755
Jack Yeh <jack.yeh@gmail.com>
2015-08-26 09:54:03 -0600

    add functionality: allow method for specific controllers and actions - the default state prevents access to every controller and action - the first statement only allows access to stores#index - the second statement gives permission to access the log in page   + needed because default access is forbidden for everything   + even guest visitors need to be given explicit permission

--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -5,5 +5,9 @@ class PermissionService
     @user = user
   end
 
+  def allow?(controller, action)
+    return true if controller == 'stores'   && action == 'index'
+    return true if controller == 'sessions' && action.in?(%w(new))
+  end
 
 end
