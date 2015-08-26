4426d670a8a673dff9fea61b01858edb475b3ccb
Jack Yeh <jack.yeh@gmail.com>
2015-08-26 10:41:23 -0600

    refactor: extract out functionality into separate methods: - extract out functionality for registered_user_permissions - extract out functionality for guest_user_permissions
    * Note: the allow method is now more readable and the flow is explicit

--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -1,17 +1,33 @@
 class PermissionService
-  attr_reader :user
+  attr_reader :user,
+              :controller,
+              :action
 
   def initialize(user)
     @user = user
   end
 
   def allow?(controller, action)
+    @controller = controller
+    @action     = action
+
     if user && user.registered_user?
-      return true if controller == 'stores'   && action.in?(%w(index show))
-      return true if controller == 'sessions' && action.in?(%w(new create destroy))
+      registered_user_permissions
     else
-      return true if controller == 'stores'   && action == 'index'
-      return true if controller == 'sessions' && action.in?(%w(new create))
+      guest_user_permissions
     end
   end
+
+  private
+
+  def registered_user_permissions
+    return true if controller == 'stores'   && action.in?(%w(index show))
+    return true if controller == 'sessions' && action.in?(%w(new create destroy))
+  end
+
+  def guest_user_permissions
+    return true if controller == 'stores'   && action == 'index'
+    return true if controller == 'sessions' && action.in?(%w(new create))
+  end
+
 end
