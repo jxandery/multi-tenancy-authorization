0079a0c2a3c192d3984f6e6df79c52acb1280439
Jack Yeh <jack.yeh@gmail.com>
2015-08-26 09:45:03 -0600

    add functionality: create current_permission method: - use the PermissionService object and the current user
    * note: the PermissionService object and folder were created to specifically to handle permissions
    ** note: that the current user must be logged in order to be valid, otherwise it returns nil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def current_permission
+    @current_permission ||= PermissionService.new(current_user)
+  end
+
+
 end
